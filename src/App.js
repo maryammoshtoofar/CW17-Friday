fdf4c36d3f0f012d770e21a6a623738451691f55
marziehmokhtari <marziehmokhtari67@gmail.com>
2022-07-22 16:26:29 +0430

    q3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import CalcRect from "./components/Q3"; 
 // import ShowVideo from "./components/Q1";
 import { Form } from "./components/Q2";
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <div className="wrapper">
       {/* <ShowVideo /> */}
-      <Form />
+      {/* <Form /> */}
+      <CalcRect/>
     </div>
   );
 }
